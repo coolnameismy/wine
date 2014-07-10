7bcedac51699638bc3a52ef7171a7e5d12ee462b
coolnameismy <coolnameismy@hotmail.com>
2014-07-10 09:04:05 +0800

    1

---
+++ src/WineWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WineWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
