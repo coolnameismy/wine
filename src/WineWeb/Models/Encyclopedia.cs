1fb9978461f4684451ee850c8b14ee2ebe7644f4
刘彦玮 <coolnameismy@hotmail.com>
2014-07-13 09:52:41 +0800

    123

---
+++ src/WineWeb/Models/Encyclopedia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WineWeb.Models
+{
+    public class Encyclopedia
+    {
+        public string Id { get; set; }
+        public string thum { get; set; }
+        public DateTime date { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+    }
+}
