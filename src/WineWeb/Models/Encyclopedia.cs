b8871febf1253a4389f3de94b9489bf8d292390e
刘彦玮 <coolnameismy@hotmail.com>
2014-07-20 22:11:13 +0800

    add validation for encyclopediamanage

--- src/WineWeb/Models/Encyclopedia.cs
+++ src/WineWeb/Models/Encyclopedia.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace WineWeb.Models
 {
     //酒事百科 
     public class Encyclopedia
     {
+        
         private DateTime _date { get; set; }
+
+        [Required(ErrorMessage = "必填字段")]
         public string Id { get; set; }
+
+        [Required(ErrorMessage = "必填字段")]
         public string thum { get; set; }
+
+        [Required(ErrorMessage = "必填字段")]
         public DateTime date {
             get {
                 return _date;
@@ -19,9 +27,18 @@ namespace WineWeb.Models
                 _date = value; 
             }
         }
+        [Required(ErrorMessage = "必填字段")]
+        [StringLength(50, ErrorMessage = "长度不能超过 {1} 个中文或英文字符。")]
         public string title1 { get; set; }
+
+        [Required(ErrorMessage = "必填字段")]
+        [StringLength(250, ErrorMessage = "长度不能超过 {1} 个中文或英文字符。")]
         public string title2 { get; set; }
+
+        [Required(ErrorMessage = "必填字段")]
+        [StringLength(2000, ErrorMessage = "长度不能超过 {1} 个中文或英文字符。")]
         public string content { get; set; }
+
         public string Link
         {
             get { return "Encyclopedia/Details/" + this.Id; }
