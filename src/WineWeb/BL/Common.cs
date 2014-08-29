9af9a5fc5b7f83b15675dc1b357f297e71e2c32e
coolnameismy <coolnameismy@hotmail.com>
2014-08-29 17:37:37 +0800

    tongbu wenjian

--- src/WineWeb/BL/Common.cs
+++ src/WineWeb/BL/Common.cs
@@ -24,8 +24,8 @@ namespace WineWeb.BL
         static public MongoDatabase GetDatabase(string databaseName)
         {
             //  MongoDB连接串，以[mongodb: // ]开头。这里，我们连接的是本机的服务 默认端口27017
-            string connectionString = "mongodb://localhost";
-            //string connectionString = "mongodb://61.147.124.16";
+            //string connectionString = "mongodb://localhost";
+            string connectionString = "mongodb://61.147.124.16";
             //  连接到一个MongoServer上 
             MongoServer server = MongoServer.Create(connectionString);
             //  打开数据库testdb 
